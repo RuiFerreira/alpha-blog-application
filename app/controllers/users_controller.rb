8aa93d2db88785f2793df18ad3b8c1a5da61c0ad
RuiFerreira <ruip.b.a.ferreira@gmail.com>
2022-07-01 15:46:18 +0100

    Added controller restrictions based on session

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,12 @@
 class UsersController < ApplicationController
-
   before_action :set_user, only: %I[show edit update]
+  before_action :require_user, only: %I[edit update]
+  before_action :require_same_user, only: %I[edit update]
 
   def show
     @articles = @user.articles.paginate(page: params[:page], per_page: 2)
   end
-  
+
   def index
     @users = User.paginate(page: params[:page], per_page: 1)
   end
@@ -44,5 +45,12 @@ class UsersController < ApplicationController
 
   def set_user
     @user = User.find(params[:id])
+  end
+
+  def require_same_user
+    if current_user != @user
+      flash[:alert] = 'You can only edit your own account'
+      redirect_to @user
+    end
   end
 end
